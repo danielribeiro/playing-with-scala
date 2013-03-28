36252cc50eda7e790fe6ca2d91f54ab8cea486c6
Daniel Ribeiro <danielrb@pagerduty.com>
2013-03-27 22:17:13 -0700

    using a different spec kind

--- test/mypackage/ExampleSpec.scala
+++ test/mypackage/ExampleSpec.scala
@@ -1,18 +1,10 @@
 package mypackage
 
-import org.scalatest.FeatureSpec
+import org.scalatest.{FunSpec, FeatureSpec}
 import org.scalatest.matchers.ShouldMatchers
 
-class ExampleSpec extends FeatureSpec with ShouldMatchers {
-
-  feature("The user can pop an element off the top of the stack") {
-
-    scenario("pop is invoked on a non-empty stack") (pending)
-
-    scenario("pop is invoked on an empty stack") (pending)
-
-    scenario("i can add two numbers") {
-      (2 + 40) should equal(42)
-    }
+class ExampleSpec extends FunSpec with ShouldMatchers {
+  it("can add two numbers") {
+      (2 + 40) should equal(43)
   }
 }
